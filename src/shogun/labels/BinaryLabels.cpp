9713979fee253a1b038328f52062679f57b34b36
Thoralf Klein <thoralf@fischlustig.de>
2014-03-14 09:17:48 +0100

    Adding missing preamble to BinaryLabels.cpp.

--- src/shogun/labels/BinaryLabels.cpp
+++ src/shogun/labels/BinaryLabels.cpp
@@ -1,3 +1,15 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 1999-2009 Soeren Sonnenburg
+ * Written (W) 1999-2008 Gunnar Raetsch
+ * Written (W) 2011-2012 Heiko Strathmann
+ * Copyright (C) 1999-2009 Fraunhofer Institute FIRST and Max-Planck-Society
+ */
+
 #include <shogun/labels/DenseLabels.h>
 #include <shogun/labels/BinaryLabels.h>
 #include <shogun/mathematics/Statistics.h>
